6a20690ab5e3db45fa473f60b2425d9667109381
VegeChou <zhouweizhi@sensoro.com>
2015-04-23 16:14:25 +0800

    Update Broadcast Key

--- Samples/Yunzi-Android/src/com/sensoro/experience/tool/MyApp.java
+++ Samples/Yunzi-Android/src/com/sensoro/experience/tool/MyApp.java
@@ -20,7 +20,6 @@ public class MyApp extends Application {
 		sensoroManager = SensoroManager.getInstance(getApplicationContext());
 		sensoroManager.setCloudServiceEnable(false);
 		sensoroManager.addBroadcastKey("01Y2GLh1yw3+6Aq0RsnOQ8xNvXTnDUTTLE937Yedd/DnlcV0ixCWo7JQ+VEWRSya80yea6u5aWgnW1ACjKNzFnig==");
-		
 		try {
 			sensoroManager.startService();
 		} catch (Exception e) {
